12bc1ca5bcb5e503f204e8959ac72e655808a629
StefanKaltschik89 <stefan.kaltschik@donau-uni.ac.at>
2022-12-07 23:08:04 +0100

    jhj

--- Auswertung.R
+++ Auswertung.R
@@ -5,22 +5,14 @@ library(tidyverse)
 library(stringr)
 setwd("~/R/Kamp/rawdata/ändern")
 
+##Liste der Filenamen im Ordner erstellen
 files <- list.files(path = "~/R/Kamp/rawdata/ändern", pattern = ".csv")
 
-# df <- list.files(path="~/R/Kamp/rawdata/ändern", pattern=".csv") %>%
-#     lapply(read_csv2, skip=3, show_col_types=F) %>%
 
-
-y<-lapply(seq_along(files), function(x) transform(read_csv2(files[x], skip=3), file = files[x])) %>%
-    bind_rows() %>%
-    mutate(file = str_replace_all(file,"Kamp_", ""))
-
-
-library(stringr)
-
-
-Kamegg<-read.csv(files[1],sep=";",skip=3,header=T, stringsAsFactors = T)
-
-files[2]
+revier_rosenburg<-lapply(seq_along(files), function(x) transform(read_delim(files[x], skip=3,), Ort = files[x])) %>% ###Lese alle files ein und füge column Ort hinzu wobei column der Name der Datei ist
+    bind_rows() %>% ###Zusammenführen der DAtensätze
+    mutate(Ort = str_replace_all(Ort,"Kamp_", "")) %>% ####Kamp und Dateieindung aus Reihen entfernen
+    mutate(Ort = str_replace_all(Ort,".csv", "")) %>%
+    select(-7) ###unnötige column Unit entfernen
 
 
